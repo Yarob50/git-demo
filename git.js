a8a6a989c87f97e0e8732f06f3f2931837bfe852
يعرب المصطفى <y3rbalmstfa@yrbs-MacBook-Pro.local>
2017-06-27 12:08:34 +0300

    adding all the files

---
+++ git.js
@@ -0,0 +1,36 @@
+/*
+
+- to make sure that git is installed : 
+	git --version
+- set up user name and email : 
+	git config --global user.name 	yarob
+	git config --global user.email 	yarob.hm@gmail.com
+
+- to check now : 
+	git config user.name
+
+- to remove directory in the command line : 
+	rmdir name
+
+- git tracks only the files where git file exists
+- commits are save points 
+
+- the 3 states of the file : 
+1- modified : 
+	when the file is modified but not commited
+
+2- staging : 
+	when the file is added to the list of commit to be commited in the next commit
+
+3- committed :
+	when it's committed :) 
+
+-to create a new repo :  
+	git init
+
+- git status : to see the status of the files 
+- to stage the modified files now : git add filename.ext
+- to add all of the files : git add .
+- to remove file from the staging area : git rm --cashed index.html
+
+*/
---
+++ gitIntro.js
@@ -0,0 +1,32 @@
+/*
+
+- to make sure that git is installed : 
+	git --version
+- set up user name and email : 
+	git config --global user.name 	yarob
+	git config --global user.email 	yarob.hm@gmail.com
+
+- to check now : 
+	git config user.name
+
+- to remove directory in the command line : 
+	rmdir name
+
+- git tracks only the files where git file exists
+- commits are save points 
+
+- the 3 states of the file : 
+1- modified : 
+	when the file is modified but not commited
+
+2- staging : 
+	when the file is added to the list of commit to be commited in the next commit
+
+3- committed :
+	when it's committed :) 
+
+-to create a new repo :  
+	git init
+
+
+*/
